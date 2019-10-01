178354f83e784a3a43ee4183a63503636a368d6c
Sumotra <sumotra@mail.ru>
2019-10-01 21:27:05 +0700

    Add to-top button

--- src/js/common.js
+++ src/js/common.js
@@ -1,3 +1,4 @@
+// Menu button
 let btn = document.getElementById('menuBtn');
 let menu = document.querySelector('.menu');
 
@@ -8,3 +9,31 @@ function toggleMenu() {
 }
 
 btn.addEventListener('click', toggleMenu);
+
+// To-top button
+(function() {
+
+  var toTopBtn = document.querySelector('.back-to-top');
+
+  function trackScroll() {
+    var scrolled = window.pageYOffset;
+    var pageHeight = document.documentElement.clientHeight;
+
+    if (scrolled > pageHeight) {
+      toTopBtn.classList.add('back-to-top-show');
+    }
+    if (scrolled < pageHeight) {
+      toTopBtn.classList.remove('back-to-top-show');
+    }
+  }
+
+  function backToTop() {
+    if (window.pageYOffset > 0) {
+      window.scrollBy(0, -80);
+      setTimeout(backToTop, 0);
+    }
+  }
+
+  window.addEventListener('scroll', trackScroll);
+  toTopBtn.addEventListener('click', backToTop);
+})();
